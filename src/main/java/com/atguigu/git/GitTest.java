ab00e0610564c876e0f4642af1bd21b8f879a53c
zjr <zjr@atguigu.com>
2023-07-11 09:59:44 +0800

    第三次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git01...");
+        System.out.println("git02...");
+        System.out.println("git03...");
     }
 }
