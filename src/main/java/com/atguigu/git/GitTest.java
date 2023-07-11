5c397fdf0c54f093d1f69002f0f8a2cda9d1a525
zjr <zjr@atguigu.com>
2023-07-11 10:15:27 +0800

    master测试冲突

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("git03...");
         System.out.println("git04...");
         System.out.println("hot-fix 1...");
+        System.out.println("master 测试冲突...");
 
     }
 }
